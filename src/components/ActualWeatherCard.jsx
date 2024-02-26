5bffa98bc5e25154a4cebe5b27f98e3adb56a5ae
joseorozco84 <joseorozco84@gmail.com>
2024-02-26 01:08:04 -0300

    ui changes

--- src/components/ActualWeatherCard.jsx
+++ src/components/ActualWeatherCard.jsx
@@ -23,21 +23,23 @@ const ActualWeatherCard = ({ weatherData }) => {
             <div className='weather-image'>
                 <img src={weatherData.current.condition.icon} alt='weather' className='icon' />
             </div>
+            
             <div className='weather-temp' onClick={toggleTemperatureUnit}>
+            <i className="bi bi-thermometer-half" style={{ fontSize: '60px', color: '#dd8888' }}></i>
                 {Math.floor(temperature)}{temperatureUnit}
             </div>
             <div className='weather-city'>{weatherData.location.name}</div>
             <div className='weather-country'>{weatherData.location.country}</div>
             <div className='data-container'>
                 <div className='element'>
-                    <i className="bi bi-water" style={{ fontSize: '60px' }}></i>
+                    <i className="bi bi-water" style={{ fontSize: '60px', color: '#0dcaf0' }}></i>
                     <div className='data'>
                         <div className='humidity-percent'>{weatherData.current.humidity}%</div>
                         <div className='text'>Humidity</div>
                     </div>
                 </div>
                 <div className='element' onClick={toggleWindSpeedUnit} >
-                    <i className="bi bi-wind" style={{ fontSize: '60px' }}></i>
+                    <i className="bi bi-wind" style={{ fontSize: '60px', color: '#bdbdbd' }}></i>
                     <div className='data'>
                         <div className='wind-rate'>
                             {Math.floor(windSpeed)}{windSpeedUnit}
